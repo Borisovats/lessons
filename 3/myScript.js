eb59133d56cb860311c34834e00c8188d173cff4
Blue <Tanechka_blue@mail.ru>
2026-01-13 01:14:33 +0300

    одна снежинка с рандомным размером и рандомным появлением

--- 3/myScript.js
+++ 3/myScript.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
     let snow = document.getElementById('snow');
-    let topPosition = 0;
+    let randomWidth = Math.random() * 100; // ширина 
+    let randomHeight = randomWidth / 3 * 2;  // высота с учетом соотношения снежинки 3 к 2 
+    let topPosition = 0; 
+    randomLeft = Math.random() * window.innerWidth; // позиция слева 
     setInterval(() => {
+        snow.style.left =  randomLeft + 'px';
         topPosition++;
+        snow.style.width = randomWidth + 'px';
+        snow.style.height = randomHeight + 'px';
         snow.style.top = topPosition + 'px';
-        bottomWindow = window.innerHeight
+        bottomWindow = window.innerHeight // конец страницы
         if (topPosition >= bottomWindow){
             topPosition = 0
         }
-    }, 30);
-
-});
+    }, 2); 
+    }
+);
