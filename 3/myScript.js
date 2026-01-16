b5935ec0f856fb554baaadb12fdb337bb0561ec2
Blue <Tanechka_blue@mail.ru>
2026-01-17 01:23:50 +0300

    40  снежинок + анимация

--- 3/myScript.js
+++ 3/myScript.js
@@ -1,19 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let snow = document.getElementById('snow');
-    let randomWidth = Math.random() * 100; // ширина 
-    let randomHeight = randomWidth / 3 * 2;  // высота с учетом соотношения снежинки 3 к 2 
-    let topPosition = 0; 
-    randomLeft = Math.random() * window.innerWidth; // позиция слева 
+document.addEventListener("DOMContentLoaded", function () {
+  for (let s = 0; s <= 40; s++) {
+    let flake = createSnow();
+    fall(flake);
+  }
+
+
+  function createSnow() {
+    // создаст  и вернет snowFlake
+    let snowFlake = document.createElement("img"); // создаст картинку
+    snowFlake.src = "snow.png"; // по адресу
+    snowFlake.className = "snow"; // присвоит имя класса
+    let randomWidth = Math.random() * 40; // ширина
+    let randomHeight = (randomWidth / 3) * 2; // высота с учетом соотношения картинки 3 к 2
+    let randomLeft = Math.random() * window.innerWidth; // позиция слева
+    snowFlake.style.left = randomLeft + "px";
+    snowFlake.style.width = randomWidth + "px";
+    snowFlake.style.height = randomHeight + "px";
+    document.body.appendChild(snowFlake); // добавит на стрпницу
+    return snowFlake;
+  }
+
+
+  function fall(flake) {
+    // flake падает
+    let topPosition = -50; //чтобы было не видно начала
+    let speed = Math.random() * 40; // скорость падения, если одинаковая они все вместе падают сугробом =)
     setInterval(() => {
-        snow.style.left =  randomLeft + 'px';
-        topPosition++;
-        snow.style.width = randomWidth + 'px';
-        snow.style.height = randomHeight + 'px';
-        snow.style.top = topPosition + 'px';
-        bottomWindow = window.innerHeight // конец страницы
-        if (topPosition >= bottomWindow){
-            topPosition = 0
-        }
-    }, 2); 
-    }
-);
+      topPosition++;
+      flake.style.top = topPosition + "px";
+      let bottomWindow = window.innerHeight; // конец страницы
+      if (topPosition >= bottomWindow) {
+        topPosition = -50;
+        let randomLeft = Math.random() * window.innerWidth; 
+        flake.style.left = randomLeft + "px";// позиция слева меняется
+      }
+    }, speed);
+  }
+});
