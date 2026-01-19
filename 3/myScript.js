065490ba5d912afb4477bf79a77fe33702fad190
Blue <Tanechka_blue@mail.ru>
2026-01-19 23:57:56 +0300

    Добавила создание снежинок по пустому месту и удаление снежинок, создание и анимацию объединила в одну функцию

--- 3/myScript.js
+++ 3/myScript.js
@@ -1,39 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-  for (let s = 0; s <= 40; s++) {
-    let flake = createSnow();
-    fall(flake);
-  }
-
-
-  function createSnow() {
-    // создаст  и вернет snowFlake
-    let snowFlake = document.createElement("img"); // создаст картинку
-    snowFlake.src = "snow.png"; // по адресу
-    snowFlake.className = "snow"; // присвоит имя класса
-    let randomWidth = Math.random() * 40; // ширина
-    let randomHeight = (randomWidth / 3) * 2; // высота с учетом соотношения картинки 3 к 2
-    let randomLeft = Math.random() * window.innerWidth; // позиция слева
-    snowFlake.style.left = randomLeft + "px";
-    snowFlake.style.width = randomWidth + "px";
-    snowFlake.style.height = randomHeight + "px";
-    document.body.appendChild(snowFlake); // добавит на стрпницу
-    return snowFlake;
-  }
-
-
-  function fall(flake) {
-    // flake падает
-    let topPosition = -50; //чтобы было не видно начала
-    let speed = Math.random() * 40; // скорость падения, если одинаковая они все вместе падают сугробом =)
-    setInterval(() => {
-      topPosition++;
-      flake.style.top = topPosition + "px";
-      let bottomWindow = window.innerHeight; // конец страницы
-      if (topPosition >= bottomWindow) {
-        topPosition = -50;
-        let randomLeft = Math.random() * window.innerWidth; 
-        flake.style.left = randomLeft + "px";// позиция слева меняется
-      }
-    }, speed);
+  for (let s = 0; s < 40; s++) {
+    createSnow();
   }
 });
+
+document.addEventListener("click", function (event) {
+  // новыя снежинка 
+  if (! event.target.closest(".snow")){ // если клик не по снежинке
+  createSnow(event)};
+});
+
+function createSnow(event) {
+  // создаст snowFlake
+  let snowFlake = document.createElement("img"); // создаст картинку
+  snowFlake.src = "snow.png"; // по адресу
+  snowFlake.className = "snow"; // присвоит имя класса
+  let randomWidth = Math.random() * 40; // ширина
+  let randomHeight = (randomWidth / 3) * 2; // высота с учетом соотношения картинки 3 к 2
+  if (event) { // если был event создаст на месте клика
+    snowFlake.style.left = event.clientX + "px";
+    snowFlake.style.top = event.clientY + "px";
+  } else {
+    let randomLeft = Math.random() * window.innerWidth; // позиция слева
+    snowFlake.style.left = randomLeft + "px";
+    snowFlake.style.top = "-50px";
+  }
+
+  snowFlake.style.width = randomWidth + "px";
+  snowFlake.style.height = randomHeight + "px";
+  document.body.appendChild(snowFlake); // добавит на стрпницу
+
+  let topPosition;
+  if (event) {
+    topPosition = event.clientY;
+  } else {
+    topPosition = -50;
+  }
+  let speed = Math.random() * 40; // скорость падения, если одинаковая они все вместе падают сугробом =)
+  snowFlake.addEventListener("click", function () { // удалит через сек при клике
+    setTimeout(() => {
+      snowFlake.remove();
+    }, 1000);
+  });
+  setInterval(() => {
+    topPosition++;
+    snowFlake.style.top = topPosition + "px";
+    let bottomWindow = window.innerHeight; // конец страницы
+    if (topPosition >= bottomWindow) {
+      topPosition = -50;
+      let randomLeft = Math.random() * window.innerWidth;
+      snowFlake.style.left = randomLeft + "px"; // позиция слева меняется
+    }
+  }, speed);
+  
+}
